2a1e29cc67eb469fde9e828d256f05d130daf7ed
Saurav207 <saurav2071996@gmail.com>
2021-06-08 10:15:38 +0530

    user Loging - 1

--- src/component/Login.js
+++ src/component/Login.js
@@ -3,13 +3,31 @@ import React, { Component } from 'react';
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
+
+  handleEmailInput = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordInput = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(' this.emailInputRef', this.emailInputRef);
-    console.log('this.passwordInputRef', this.passwordInputRef);
+    // console.log(' this.emailInputRef', this.emailInputRef);
+    // console.log('this.passwordInputRef', this.passwordInputRef);
+    console.log('this.state', this.state);
   };
 
   render() {
@@ -21,7 +39,9 @@ class Login extends Component {
             type="email"
             placeholder="Email"
             required
-            ref={this.emailInputRef}
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailInput}
+            value={this.state.email}
           />
         </div>
         <div className="field">
@@ -29,7 +49,9 @@ class Login extends Component {
             type="password"
             placeholder="Password"
             required
-            ref={this.passwordInputRef}
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordInput}
+            value={this.state.password}
           />
         </div>
         <div className="field">
